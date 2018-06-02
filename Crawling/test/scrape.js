ef91e17c2e23fad9d734d487fc64948034d51be2
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-06-02 14:52:39 -0500

    getting some data points

--- Crawling/test/scrape.js
+++ Crawling/test/scrape.js
@@ -2,32 +2,62 @@ const cheerio = require('cheerio');
 const request = require('request');
 const uri = 'https://www.walmart.com/search/?query=Books&redirect=false';
 
+const books = []
+
 const scrape = function() {
   request(uri, function(error, response, html) {
     let $ = cheerio.load(html);
 
-    $('div').each(function(i, el) {
-      let node = $(el).attr('data-tl-id')
+    //  Let's build a book
+    let newBook = {
+      id: '',
+      name: '',
+      listPrice: '',
+      description: '',
+      product_dimension: '',
+      imageURLs: '',
+      weight: '',
+      sourceURL: ''
+    }
+    // getting Products
+    $('div[data-tl-id]').each(function(i, el) {
+      // console.log($(this).html())
 
-      if (node = `ProductTileListView-${i}`) {
-        console.log(node)
-      }
+      let top = $(el).data('tl-id', `ProductTileListView-${i}`)
+      // console.log($(top).html())
+
+      let thing1 = $(top).children('div.search-result-listview-item')
+      // console.log($(thing1).html())
+
+      let thing2 = $(thing1).children('div')
+      // console.log($(thing2).html())
+
+      let data1 = $(thing2).children('div.Grid-col')
+      // console.log($(data1).html())
+
+      let data1_ = $(data1).children('div.search-result-productimage')
+      // console.log($(data1_).html())
+
+      let data1__ = $(data1_).children('div.display-inline-block')
+      // console.log($(data1__).html())
+
+      let data1___ = $(data1__).children('a')
+      // COME BACK TO THIS ONCE PASSING URL STR FROM NAVJS
+      // let sourceURL = url + $(data1___).attr('href')
+      // console.log(sourceURL)
+
+      // HERE IS THE TITLE OF THE BOOK
+      let title = $(data1___).children('.Tile-img').attr('alt')
+      // console.log(title)
+
+      // HERE IS THE IMG LINK
+      let imgLink = $(data1___).children('.Tile-img').attr('src')
+      // console.log(imgLink)
+
+      let data2 = $(data1).next('div.Grid-col')
+      console.log($(data2).html())
       
     })
-    // for(n=0; n<10; n++) {
-    //   $(`div[data-tl-id="ProductTileListView-${n}"]`).each(function(i, el) {
-    //     let node = $(el)
-    //     console.log(node)
-    //   });
-    // }
-
-    // $('.search-result-listview-items').each(function(i, element) {
-    //   // let title = $(element).attr('title')
-
-    //   let node =  $(element).children('div').attr('data-tl-id')// === 'ProductTileListView-'+i
-    //   console.log(node)
-    // })
-
   })
 }
 
